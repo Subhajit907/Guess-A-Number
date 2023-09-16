4276af1772d7d2d8e37705d3e4160e57c2a39bc3
Subhajit Mandal <78304113+Subhajit907@users.noreply.github.com>
2023-09-16 14:58:43 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,18 @@
+// Generate a random number between 1 and 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
 
+function checkGuess() {
+    const guess = parseInt(document.getElementById('guessInput').value);
+    attempts++;
+
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+        document.getElementById('result').innerHTML = 'Please enter a valid number between 1 and 100.';
+    } else if (guess === randomNumber) {
+        document.getElementById('result').innerHTML = `Congratulations! You guessed the number ${randomNumber} in ${attempts} attempts.`;
+    } else if (guess < randomNumber) {
+        document.getElementById('result').innerHTML = 'Try a higher number.';
+    } else {
+        document.getElementById('result').innerHTML = 'Try a lower number.';
+    }
+}
